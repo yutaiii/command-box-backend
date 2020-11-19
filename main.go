ba22226aa86d120076b9ffce250d66adffd84b2f
市川裕太 <yu74ichi69@gmail.com>
2020-11-19 23:27:05 +0900

    echoを使用する様に変更

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
 
 func main() {
-	fmt.Println("hello world")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//routing
+	e.GET("/hello", hello())
+
+	//exec server
+	e.Start(":8000")
+}
+
+func hello() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello")
+	}
 }
